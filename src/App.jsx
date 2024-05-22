ec542199f5e1e66e3cf643f3746b2f43b48aa2be
Ikram <ikram.ram1501@gmail.com>
2024-05-22 12:50:13 +0700

    todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     {
       id: 1,
       title: 'Finish Progate React Course',
-      completed: false,
+      completed: false,  // Menganti nilai awal ke false lagi
     },
     {
       id: 2,
@@ -20,12 +20,23 @@ function App() {
     },
   ])
 
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
   console.log(todos)
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} />
+      {/* Teruskan function toggleCompleted ke component Todos */}
+      <Todos todos={todos} toggleCompleted={toggleCompleted} />
     </div>
   )
 }
